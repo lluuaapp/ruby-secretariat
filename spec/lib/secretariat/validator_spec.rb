4e916ea01a99ed5358d7625cf7a63a6aca2887b4
Sascha Skulima <218423+Skulli@users.noreply.github.com>
2024-06-26 10:33:34 +0200

    rewrite with rspec

---
+++ spec/lib/secretariat/validator_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+RSpec.describe Secretariat::Validator do
+
+  context "zugpferd2 schema extended" do
+    let(:xml) { File.open(Secretariat.file_path("spec/fixtures/zugferd_2/extended.xml")) }
+    subject { described_class.new(xml, version: 2) }
+
+    it {
+      expect(subject.validate_against_schema).to be_empty
+    }
+  end
+Secretariat
+  context "zugpferd2 schematron extended" do
+    let(:xml) { File.open(Secretariat.file_path("spec/fixtures/zugferd_2/extended.xml")) }
+    subject { described_class.new(xml, version: 2) }
+
+    it {
+      expect(subject.validate_against_schematron).to be_empty
+    }
+  end
+
+  context "zugpferd1 schema extended" do
+    let(:xml) { File.open(Secretariat.file_path("spec/fixtures/zugferd_1/einfach.xml")) }
+    subject { described_class.new(xml, version: 1) }
+
+    it {
+      expect(subject.validate_against_schema).to be_empty
+    }
+  end
+
+  context "zugpferd1 schematron extended" do
+    let(:xml) { File.open(Secretariat.file_path("spec/fixtures/zugferd_1/einfach.xml")) }
+    subject { described_class.new(xml, version: 1) }
+
+    it {
+      expect(subject.validate_against_schematron).to be_empty
+    }
+  end
+end
--- test/test_helper.rb
+++
@@ -1,2 +0,0 @@
-require_relative "../lib/secretariat"
-require "minitest/autorun"
