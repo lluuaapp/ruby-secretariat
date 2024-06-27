828f8954bbb144741e5e3645b6be47f158581bc3
Sascha Skulima <218423+Skulli@users.noreply.github.com>
2024-06-27 08:57:14 +0200

    chg: define invoice-timeframe

--- lib/secretariat/line_item.rb
+++ lib/secretariat/line_item.rb
@@ -31,6 +31,8 @@ module Secretariat
     :origin_country_code,
     :currency_code,
     :buyer_id,
+    :invoice_start,
+    :invoice_end,
     keyword_init: true) do
     include Versioner
 
@@ -158,6 +160,20 @@ module Secretariat
 
             percent = by_version(version, "ApplicablePercent", "RateApplicablePercent")
             xml["ram"].send(percent, Helpers.format(tax_percent))
+          end
+          if invoice_start.to_s != "" && invoice_end.to_s != ""
+            xml["ram"].BillingSpecifiedPeriod do
+              xml["ram"].StartDateTime do
+                xml["udt"].DateTimeString(format: "102") do
+                  xml.text(invoice_start.strftime("%Y%m%d"))
+                end
+              end
+              xml["ram"].OccurrenceDateTime do
+                xml["udt"].EndDateTime(format: "102") do
+                  xml.text(invoice_end.strftime("%Y%m%d"))
+                end
+              end
+            end
           end
           monetary_summation = by_version(version, "SpecifiedTradeSettlementMonetarySummation", "SpecifiedTradeSettlementLineMonetarySummation")
           xml["ram"].send(monetary_summation) do
